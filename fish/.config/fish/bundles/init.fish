67b534294c57039bf204e81905f3a43ad6227ae0
Jamie Schembri <jamie@lico.nl>
2017-04-15 02:01:09 +0200

    Add windows bundles (fish-ssh-agent)

--- fish/.config/fish/bundles/init.fish
+++ fish/.config/fish/bundles/init.fish
@@ -2,5 +2,6 @@ fundle plugin 'shkm/vssh'
 fundle plugin 'shkm/fish-vimple'
 
 [ (uname) = Darwin ]; and source $HOME/.config/fish/bundles/macos.fish
+[ (uname -r | grep Microsoft) ]; and source $HOME/.config/fish/bundles/windows.fish
 
 fundle init
