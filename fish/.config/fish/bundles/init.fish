fba92093ef5b7479d77c90f09895ab2fa8a77213
Jamie Schembri <jamie@lico.nl>
2017-04-26 19:42:04 +0200

    Add rvm

--- fish/.config/fish/bundles/init.fish
+++ fish/.config/fish/bundles/init.fish
@@ -1,5 +1,6 @@
 fundle plugin 'shkm/vssh'
 fundle plugin 'shkm/fish-vimple'
+fundle plugin 'fisherman/rvm'
 
 [ (uname) = Darwin ]; and source $HOME/.config/fish/bundles/macos.fish
 [ (uname -r | grep Microsoft) ]; and source $HOME/.config/fish/bundles/windows.fish
