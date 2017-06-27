b213b95eb00fd7f1a2a5ed1bd81c17f1875b724c
Jamie Schembri <jamie@lico.nl>
2017-06-27 22:42:47 +0200

    Update sourceables

---
+++ zsh/.zsh/init/sourceables.sh
@@ -0,0 +1,3 @@
+source /Users/jamie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source /Users/jamie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+source /Users/jamie/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
