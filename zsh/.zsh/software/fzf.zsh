8eecb8877840ea3b7e4facebee383243993711a7
Jamie Schembri <jamie@lico.nl>
2017-09-15 15:05:56 +0200

    fzf script changes

--- zsh/.zsh/software/fzf.zsh
+++ zsh/.zsh/software/fzf.zsh
@@ -54,17 +54,17 @@ repos() {
 }
 
 # fssh - ssh into a host specified in ~/.ssh/config
+# Takes an argument which will instead execute that command.
 fssh() {
+  local command="${1}"
   local host
   host=$(grep '^host .*' ~/.ssh/config | cut -d ' ' -f 2 | fzf)
-  ssh $host
+
+  [ -z $host ] && return
+
+  echo "Connecting to ${host}"
+  [ -n $command ] && echo "Command: ${command}"
+
+  ssh $host $command
 }
-
-frconsole() {
-  local host
-  host=$(grep ':$'  ~/.rconsole | cut -d ':' -f 1 | fzf)
-  rconsole $host
-}
-
-
 # }}}
