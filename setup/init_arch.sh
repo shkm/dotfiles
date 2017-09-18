2f30021624240a0743c219266d352d67ef331ade
Jamie Schembri <jamie@lico.nl>
2017-09-18 17:45:38 +0200

    Improve init_arch

--- setup/init_arch.sh
+++ setup/init_arch.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
-sudo pacman -S --noconfirm --needed yaourt stow zsh tig ripgrep tmux xclip fzf \
+sudo pacman -S --noconfirm --needed yaourt stow zsh tig ripgrep tmux xclip \
   neovim python2-neovim python-neovim \
   go
 
-mkdir ~/bin
+mkdir ~/bin 2>/dev/null
 
-go get antibody
+go get github.com/getantibody/antibody
 
 if [ ! -e ~/dotfiles ]; then
   git clone https://github.com/shkm/dotfiles.git ~/dotfiles
-  rm ~/.bash_profile # probably already exists, so remove before stowing
+  rm ~/.bash_profile 2>/dev/null # probably already exists, so remove before stowing
   stow -d ~/dotfiles ag alacritty autokey bash eslint git scripts tig tmux vim zsh
 fi
 
-chsh -s /usr/bin/zsh
+if ! command -v diff-highlight > /dev/null; then
+  sudo easy_install diff-highlight
+fi
+
+[ "$SHELL" = '/usr/bin/zsh' ] || chsh -s /usr/bin/zsh
 
