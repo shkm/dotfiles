c9aefc10a41ab4e28e8aabbce289f6fc08f2e1f5
Jamie Schembri <jamie@lico.nl>
2017-12-23 16:50:09 +0100

    Update arch init

--- setup/init_arch.sh
+++ setup/init_arch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-sudo pacman -S --noconfirm --needed yaourt stow zsh tig ripgrep tmux xclip \
-  neovim python2-neovim python-neovim \
-  go
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm --needed yaourt stow zsh tig ripgrep tmux xclip neovim python2-neovim python-neovim go tree
 
 mkdir ~/bin 2>/dev/null
 
@@ -9,8 +8,8 @@ go get github.com/getantibody/antibody
 
 if [ ! -e ~/dotfiles ]; then
   git clone https://github.com/shkm/dotfiles.git ~/dotfiles
-  rm ~/.bash_profile 2>/dev/null # probably already exists, so remove before stowing
-  stow -d ~/dotfiles ag alacritty autokey bash eslint git scripts tig tmux vim zsh
+  rm .profile ~/.bash_profile ~/.Xresources 2>/dev/null # probably already exists, so remove before stowing
+  stow -d ~/dotfiles ag alacritty autokey bash eslint git scripts tig tmux vim zsh kde x autokey sh
 fi
 
 if ! command -v diff-highlight > /dev/null; then
