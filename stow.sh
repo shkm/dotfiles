9bcbe6f7da261b9dcb4e24c83e135970f18e0e71
Jamie Schembri <jamie@lico.nl>
2018-05-14 21:37:10 +0200

    Add stow.sh

---
+++ stow.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# Stows my stuff.
+# This script's purpose is to keep a record of what I stow across platforms,
+# and provide a quick method to stow packages under specific fonditions.
+
+set -o errexit
+set -o pipefail
+
+readonly PACKAGES=( ag alacritty bash eslint git ruby scripts sh tig tmux vim zsh )
+readonly MACOS_PACKAGES=()
+readonly LINUX_PACKAGES=( autokey )
+
+
+install () {
+  local name="${1}"
+  local -a packages=("${!2}")
+
+  if [ ${#packages[@]} -ne 0 ]; then
+    printf "$(tput setaf 6)%s:$(tput sgr0)\n" "${name}"
+    for package in "${packages[@]}"; do printf "  %s\n" "${package}"; done
+
+    stow "${packages[@]}";
+  fi
+}
+
+general () {
+  install "General" PACKAGES[@]
+}
+
+macos () {
+  if [ "$(uname)" = 'Darwin' ]; then
+    install "MacOS" MACOS_PACKAGES[@]
+  fi
+}
+
+linux () {
+  if [ "$(uname)" != 'Linux' ]; then
+    install "Linux" LINUX_PACKAGES[@]
+  fi
+}
+
+main () {
+  general
+  macos
+  linux
+}
+
+main "$@"
