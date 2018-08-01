4cf53685f1114922def4263e64e0475db1867a67
Jamie Schembri <jamie@lico.nl>
2018-08-01 09:59:42 +0200

    Improve ruby alias

--- zsh/.zsh/language/ruby.zsh
+++ zsh/.zsh/language/ruby.zsh
@@ -4,5 +4,5 @@ alias cap="bundle exec cap"
 alias rails="bundle exec rails"
 alias html2haml="html2haml --ruby19-attributes --erb"
 
-alias rdbc="rails db:create"
+alias rdbc="rails db:create db:schema:load"
 alias rtdbc="RAILS_ENV=test rails db:create"
