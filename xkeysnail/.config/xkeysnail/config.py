73b9236657cb358efe8c1d232f9de4205a1a5a6f
Jamie Schembri <jamie@schembri.me>
2020-12-30 09:07:05 +0100

    Move all remaining xmodmap suff into xkeysnail

--- xkeysnail/.config/xkeysnail/config.py
+++ xkeysnail/.config/xkeysnail/config.py
@@ -1,6 +1,15 @@
 import re
 from xkeysnail.transform import *
 
+define_keymap(None, {
+    K("RM-J"): K("down"),
+    K("RM-K"): K("up"),
+    K("RM-H"): K("left"),
+    K("RM-L"): K("right"),
+    K("RM-KEY_0"): K("home"),
+    K("RM-Shift-KEY_4"): K("end")
+    }, "Right-Alt Vim")
+
 define_keymap(re.compile("konsole"), {
     K("LC-J"): launch(["konsole-vim-navigate", "down"]),
     K("LC-K"): launch(["konsole-vim-navigate", "up"]),
