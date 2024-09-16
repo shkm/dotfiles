e3c0984d00cb63cc4e4559a35a5c2d25fc7c407f
Jamie Schembri <jamie@schembri.me>
2024-09-16 09:38:12 +0200

    Update lazyvim packages

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -188,17 +188,7 @@ end
 	end
 },
 {
-	"nvim-telescope/telescope.nvim", dependencies = { 'nvim-lua/plenary.nvim' }, config = function()
-		local trouble = require("trouble.providers.telescope")
-		require("telescope").setup {
-			defaults = {
-				mappings = {
-					i = { ["<c-t>"] = trouble.open_with_trouble },
-					n = { ["<c-t>"] = trouble.open_with_trouble },
-				}
-			}
-		}
-	end
+	"nvim-telescope/telescope.nvim", dependencies = { 'nvim-lua/plenary.nvim' }
 },
 { "folke/which-key.nvim", config = function()
 	require("which-key").setup {
