80c07fa5414cfb0855a36db753bf332c760aaa0c
Jamie Schembri <jamie@schembri.me>
2025-09-17 10:13:32 +0200

    Indentation of fish abbr

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,23 +1,24 @@
 if status is-interactive
-  abbr cat bat
-  abbr dc docker compose
-  abbr dka "docker kill (docker ps -q)"
-  abbr dsa "docker stop (docker ps -q)"
-  abbr ducks "du -cksh * | sort -hr"
-  abbr g git
-  abbr gcd "cd (git rev-parse --show-toplevel)"
-  abbr gsha 'git rev-parse HEAD'
-  abbr ll eza -lga --group-directories-first
-  abbr ls ll
-  abbr sshkey "clip $HOME/.ssh/id_ed25519.pub"
-  abbr rm trash
-  abbr v nvim
-  abbr v. "nvim ."
-  abbr z. "zed ."
-  abbr vim nvim
-  abbr be bundle exec
-  abbr dcps docker compose ps --format \"table {{.ID}}\t{{.Service}}\t{{.Status}}\"
-  abbr dcpsa docker compose ps --all --format \"table {{.ID}}\t{{.Service}}\t{{.Status}}\"
-  abbr lg lazygit
-  abbr lgh lazygit log -f
+    abbr cat bat
+    abbr dc docker compose
+    abbr dka "docker kill (docker ps -q)"
+    abbr dsa "docker stop (docker ps -q)"
+    abbr ducks "du -cksh * | sort -hr"
+    abbr g git
+    abbr gcd "cd (git rev-parse --show-toplevel)"
+    abbr gsha 'git rev-parse HEAD'
+    abbr ll eza -lga --group-directories-first
+    abbr ls ll
+    abbr sshkey "clip $HOME/.ssh/id_ed25519.pub"
+    abbr rm trash
+    abbr v nvim
+    abbr v. "nvim ."
+    abbr z. "zed ."
+    abbr vim nvim
+    abbr be bundle exec
+    abbr dcps docker compose ps --format \"table {{.ID}}\t{{.Service}}\t{{.Status}}\"
+    abbr dcpsa docker compose ps --all --format \"table {{.ID}}\t{{.Service}}\t{{.Status}}\"
+    abbr lg lazygit
+    abbr lgh lazygit log -f
+    abbr art php artisan
 end
