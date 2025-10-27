41f3411e3e7d1426f150aef7a6e7d6b023a4c98c
Jamie Schembri <jamie@schembri.me>
2025-10-27 14:47:38 +0100

    [fish] add c for better cd

--- fish/.config/fish/conf.d/functions.fish
+++ fish/.config/fish/conf.d/functions.fish
@@ -1,10 +1,24 @@
 function mkcd
-  if test (count $argv) -eq 0
-    echo "Usage: mkcd <directory_name>"
-    return 1
-  end
-  mkdir -p $argv[1]
-  cd $argv[1]
+    if test (count $argv) -eq 0
+        echo "Usage: mkcd <directory_name>"
+        return 1
+    end
+    mkdir -p $argv[1]
+    cd $argv[1]
+end
+
+function c
+    set target $argv[1]
+
+    # Resolve absolute path (fail if not resolvable)
+    set -l path (realpath -- $target 2>/dev/null)
+
+    # If it's a file, use its directory; otherwise keep as-is
+    if ! test -d "$path"
+        set path (dirname -- "$path")
+    end
+
+    cd -- "$path"
 end
 
 function tempe
@@ -18,7 +32,7 @@ function tempe
 end
 
 function reload!
-  set config $__fish_config_dir/config.fish
-  echo "Reloading config from $config"
-  source $config
+    set config $__fish_config_dir/config.fish
+    echo "Reloading config from $config"
+    source $config
 end
