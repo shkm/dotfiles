01a20020c192434307dfd8764cddd5adb310fb4f
Jamie Schembri <jamie@schembri.me>
2026-02-03 14:28:54 +0100

    [fish] set title to pwd

--- fish/.config/fish/conf.d/00_init.fish
+++ fish/.config/fish/conf.d/00_init.fish
@@ -6,6 +6,10 @@ set -U fish_greeting
 # FZF's legacy keybindings conflict with Fish
 set -U FZF_LEGACY_KEYBINDINGS 0
 
+function fish_title
+    basename (pwd)
+end
+
 # A place to put transient settings that shouldn't be versioned
 if test -f $HOME/.transient/fish.fish
     source $HOME/.transient/fish.fish
