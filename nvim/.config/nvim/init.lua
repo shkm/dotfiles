4751d2ca00bf2f3fb11d2e762bf3349306cffe60
Jamie Schembri <jamie@schembri.me>
2026-02-04 09:30:01 +0100

    [nvim] Customize overseer task list display and add open url action
    - Use Nerd Font icons with status-based colors for task list
    - Add "open url" action to open task URL in browser
    - Use shkm/overseer.nvim fork until PR #494 is merged

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -835,14 +835,42 @@ require("lazy").setup({
 
   -- Overseer - task runner
   {
-    "stevearc/overseer.nvim",
-    cmd = { "OverseerOpen", "OverseerClose", "OverseerToggle", "OverseerRun" },
+    "shkm/overseer.nvim", -- TODO: switch back to stevearc/overseer.nvim after PR #494 is merged
+    branch = "fix-mix-provider-callback",
     keys = {
       { "<leader>e", nil, desc = "Execute" },
       { "<leader>er", "<cmd>OverseerRun<cr>", desc = "Run task" },
       { "<leader>eo", "<cmd>OverseerToggle<cr>", desc = "Toggle Overseer" },
     },
-    opts = {},
+    opts = {
+      disable_template_modules = { "overseer.template.mix" },
+      actions = {
+        ["open url"] = {
+          desc = "Open the task URL in browser",
+          condition = function(task)
+            return task.metadata and task.metadata.url
+          end,
+          run = function(task)
+            vim.ui.open(task.metadata.url)
+          end,
+        },
+      },
+      task_list = {
+        render = function(task)
+          local STATUS = {
+            RUNNING = { icon = "󰐊", hl = "DiagnosticInfo" },
+            SUCCESS = { icon = "󰄬", hl = "DiagnosticOk" },
+            FAILURE = { icon = "󰅖", hl = "DiagnosticError" },
+            CANCELED = { icon = "󰜺", hl = "DiagnosticWarn" },
+            PENDING = { icon = "󰔟", hl = "Comment" },
+          }
+          local s = STATUS[task.status] or { icon = "?", hl = "Normal" }
+          return {
+            { { s.icon .. " ", s.hl }, { task.name, "Normal" } },
+          }
+        end,
+      },
+    },
   },
 
   -- Mini.nvim modules
